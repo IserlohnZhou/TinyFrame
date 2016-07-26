4b365e270efbb8838dea4fd6460bb2a819d27513
root <root@localhost.(none)>
2016-07-26 15:13:26 +0800

    first commit

---
+++ framework/framework.php
@@ -0,0 +1,10 @@
+<?php
+
+//init
+define('FRAME_PATH',__DIR__.'/');
+
+require('./config/config.php');
+require(FRAME_PATH.'core.php');
+
+$core=new core;
+$core->run();
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+define('APP_PATH',__DIR__.'/');
+
+require('./framework/framework.php');
